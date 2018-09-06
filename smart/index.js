af04fc4be23df372afebece2ee0f80f4507e010a
vitya1 <v.sheremetov@b2b-center.ru>
2018-09-06 22:00:49 +0300

    rebuild smart contract service on infura

--- smart/index.js
+++ smart/index.js
@@ -3,10 +3,11 @@
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
-const net = require('net');
 const zmq = require('zeromq');
 const nconf = require('nconf');
 const Web3 = require('web3');
+const Tx = require('ethereumjs-tx');
+const keythereum = require('keythereum');
 
 const zpush = zmq.socket('push');
 const zpull = zmq.socket('pull');
@@ -15,10 +16,9 @@ nconf.argv()
     .env()
     .file(path.join(__dirname, '.env.local'))
     .defaults({
-        ipc_path: path.join(
+        eth_path: path.join(
             os.homedir(),
-            (os.platform() === 'darwin' ? 'Library/Ethereum/' : '.ethereum/'),
-            'geth.ipc'
+            (os.platform() === 'darwin' ? 'Library/Ethereum/' : '.ethereum/')
         ),
     });
 
@@ -40,41 +40,43 @@ if(!nconf.get('account_password')) {
     process.exit();
 }
 
-const ipc_path = nconf.get('ipc_path');
-const web3 = new Web3(new Web3.providers.IpcProvider(ipc_path, net));
+const account_address = nconf.get('account_address');
+const contract_address = nconf.get('contract_address');
+
+const key_object = keythereum.importFromFile(account_address, path.join(nconf.get('eth_path'), 'rinkeby'));//yet
+const private_key = keythereum.recover(nconf.get('account_password'), key_object);
 
 const abi = fs.readFileSync(path.join(__dirname, 'hesher_sol_ImgHesher.abi'));
 const abi_json = JSON.parse(abi.toString());
 
-const hesher_contract_addr = nconf.get('contract_address');
-const hesher_contract = new web3.eth.Contract(abi_json, hesher_contract_addr, {
-    gasLimit: 200000
-});
-//console.log(hesher_contract.methods);
-//console.log(web3.eth.personal.getAccounts().then(res => console.log(res)));
+const web3 = new Web3(new Web3.providers.HttpProvider(nconf.get('infura_endpoint')));
+const contract = new web3.eth.Contract(abi_json, contract_address);
 
-web3.eth.getAccounts().then(result => {
-    let main_address = result[0];
+web3.eth.getTransactionCount(account_address).then(function(nonce) {
+    let count = nonce;
 
     zpull.on('message', (msg) => {
-        web3.eth.personal.unlockAccount(main_address, nconf.get('account_password'))
-            .then(res => {
-                let data = JSON.parse(msg.toString());
-                if(!data.url || !data.hash) {
-                    console.log('Error. Message must have url and hash');
-                    return;
-                }
+        const data = JSON.parse(msg.toString());
+        if(!data.url || !data.hash) {
+            console.log('Error. Message must have url and hash');
+            return;
+        }
 
-                hesher_contract.methods.push(data.url, data.hash, '')//data.archive_hash
-                    .send({from: result[0]})
-                //    .estimateGas()
-                    .then(receipt => {
-                        //@todo get number in blockchain array
-                        zpush.send(JSON.stringify([data.name, receipt]));
-                    }).catch(err => console.log('Transaction error', err));
+        let raw_transaction = {
+            'from': account_address,
+            'to': contract_address,
+            'value': '0x0',
+            'gasPrice': web3.utils.toHex(20 * 1e9),
+            'gasLimit': web3.utils.toHex(240000),
+            'data': contract.methods.push(data.url, data.hash, '').encodeABI(),//data.archive_hash
+            'nonce': web3.utils.toHex(count++),
+        };
 
-            }).catch(auth_err => console.log('Auth error', auth_err));
-        });
+        let transaction = new Tx(raw_transaction);
+        transaction.sign(private_key);
 
-}).catch(err => console.log('Account error', err));
+        web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
+            .catch(err => console.log('Transaction error', err));
+    });
 
+}).catch(err => console.log('Tx count error', err));
